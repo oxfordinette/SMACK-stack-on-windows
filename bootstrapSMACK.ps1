094b288ae55728deb7d260b59b4406670f8af172
Yen <yenwel1987@gmail.com>
2017-10-10 16:06:22 +0200

    add kafka and DDDD template

--- bootstrapSMACK.ps1
+++ bootstrapSMACK.ps1
@@ -1,5 +1,12 @@
-choco install datastax.community 
-choco install scala
-choco install sbt
-choco install intellijidea-community
-choco install activator
+choco install datastax.community -y
+choco isntall jdk -y
+choco install scala -y
+choco install sbt -y
+choco install intellijidea-community -y
+choco install activator -y
+choco install kafka -y
+choco install git -y
+
+git clone https://github.com/boldradius/akka-dddd-template.git
+
+cd akka-dddd-template
